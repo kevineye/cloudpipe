2fc5c225d13e771291dec3159200a55755217a3a
eye <eye@buffalo.edu>
2015-06-05 22:31:13 -0400

    streaming works now

--- server.pl
+++ server.pl
@@ -1,7 +1,14 @@
 #!/usr/bin/perl
 
-# stream to this with cat | curl -s -T - -H Expect: http://localhost:3000/any/name
-# stream from this with curl -s http://localhost:3000/any/name
+# stream to this with cat | curl -fsS -T - -H Expect: http://localhost:3000/any/name
+# stream from this with curl -fsSN http://localhost:3000/any/name
+# slow-write with perl -e 'syswrite STDOUT, $_ and sleep 1 for 0..9'
+# slow-read with perl -e 'sleep 1 while sysread STDIN, $s, 1 and syswrite STDOUT, $s'
+
+# TODO add web site to list, edit, view/watch, add "copy curl", "install" options
+# TODO add other backends (e.g. pastebin, gist)
+# TODO add --edit option that uploads, waits for web edit, then re-downloads
+# TODO add expiration on files (deleted after ___ days)
 
 BEGIN {
     $ENV{MOJO_INACTIVITY_TIMEOUT} = 0;
@@ -18,7 +25,8 @@ app->log->level('debug');
 my $cloud_storage_path = $ENV{CLOUD_STORAGE} || '/tmp/cloud';
 -d $cloud_storage_path or make_path $cloud_storage_path or die "cannot create $cloud_storage_path; override with CLOUD_STORAGE environment variable\n";
 
-my %open;
+my %writing;
+my %reading;
 
 hook after_build_tx => sub {
     my $tx = shift;
@@ -35,6 +43,12 @@ hook after_build_tx => sub {
         read => sub {
             my ($content, $bytes) = @_;
             put_recv($tx->req->url->path, $bytes) if $tx->req->method eq 'PUT';
+        }
+    );
+
+    $tx->on(
+        finish => sub {
+            put_close($tx->req->url->path) if $tx->req->method eq 'PUT';
         }
     );
 };
@@ -54,37 +68,91 @@ get '*' => sub {
 
 sub put_open {
     my ($path) = @_;
-    app->log->debug("$path -> OPENED FOR WRITE");
+    app->log->debug("$path -> BEGIN RECEIVING");
     unlink "$cloud_storage_path$path" if -e "$cloud_storage_path$path";
-    my $file = $open{$path} = Mojo::Asset::File->new(path => "$cloud_storage_path$path", cleanup => 0);
+    $writing{$path} = Mojo::Asset::File->new(path => "$cloud_storage_path$path", cleanup => 0);
+    $_->() for @{$reading{$path} || []};
     # TODO what if already open?
-    # TODO notify any readers
     # TODO implement append
 }
 
 sub put_recv {
     my ($path, $data) = @_;
-    app->log->debug(sprintf "%s -> WRITE %d bytes", $path, length $data);
-    $open{$path}->add_chunk($data);
-    # TODO notify any readers
+    app->log->debug(sprintf "%s -> RECEIVED %d bytes", $path, length $data);
+    $writing{$path}->add_chunk($data);
+    $_->() for @{$reading{$path} || []};
 }
 
 sub put_close {
     my ($path) = @_;
-    app->log->debug("$path -> CLOSED FOR WRITE");
-    delete $open{$path};
-    # TODO notify any readers
+    app->log->debug("$path -> FINISHED RECEIVING");
+    delete $writing{$path};
+    $_->() for @{$reading{$path} || []};
 }
 
 sub get_send {
     my ($path, $controller) = @_;
-    app->log->debug("$path -> GET");
-    $controller->res->code(200)->content->asset(Mojo::Asset::File->new(path => "$cloud_storage_path$path"));
-    $controller->rendered;
-    #$controller->write_chunk(Mojo::Asset::File->new(path => "$cloud_storage_path$path"), sub { $controller->finish });
-    # TODO if not exists, wait until exists or closed
-    # TODO tail behavior -- should wait when sent until close and watch for more
+
+    my $file = Mojo::Asset::File->new(path => "$cloud_storage_path$path", cleanup => 0);
+    my $pos = 0;
+    app->log->debug("$path -> BEGIN SENDING");
+
+    my $cb;
+    $cb = sub {
+
+        # must have been truncated; return to beginning
+        $pos = 0 if $pos > $file->size;
+
+        # we've sent the whole file
+        if ($pos == $file->size) {
+
+            # if the file is not still being written, close it
+            unless ($writing{$path}) {
+                get_close($path, $cb);
+                $controller->finish;
+            } else {
+                app->log->debug("$path -> FINISHED SENDING, WAITING");
+            }
+
+            # if it is still being written, our cb will be activated later by the writer
+        } else {
+            # send the next chunk, and then call the callback again
+            my $data = $file->get_chunk($pos);
+            $pos += length $data;
+            app->log->debug(sprintf "%s -> SEND %d bytes", $path, length $data);
+            $controller->write_chunk($data, $cb);
+        }
+
+    };
+
+    # setup cleanup
+    $controller->on(
+        finish => sub {
+            get_close($path, $cb);
+        }
+    );
+
+    # open the file by listing it in the readers for this path
+    $reading{$path} ||= [];
+    push @{$reading{$path}}, $cb;
+
+    # call the callback to get things started
+    # unless the file doesn't exist yet... we'll do nothing now and wait
+    if ($file->size > 0) {
+        $cb->();
+    } else {
+        app->log->debug("$path -> WAITING FOR INITIAL DATA");
+    }
+
+    # TODO fix bad FD on waiting for start
     # TODO consider last=x param
+}
+
+sub get_close {
+    my ($path, $cb) = @_;
+    app->log->debug("$path -> CLOSING");
+    $reading{$path} = [ grep { $_ ne $cb } @{$reading{$path}} ];
+    delete $reading{$path} if @{$reading{$path}} == 0;
 }
 
 app->start;
